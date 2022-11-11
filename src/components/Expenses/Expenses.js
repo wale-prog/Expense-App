3a98354082fc137e7f2f297b4e1eb7ad2704d394
Olawale <olapetanwale@outlook.com>
2022-11-11 16:06:32 +0100

    Split ExpenseList to its own component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props)=> {
   const [selectedYear, setSelectedYear] = useState('2020')
@@ -13,25 +13,13 @@ const Expenses = (props)=> {
 
   const filteredExpenses = props.items.filter(expense => 
     expense.date.getFullYear() === Number(selectedYear)
-  );
-
-  let expenseContent =  <p>No expense found.</p>
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map(expense =>
-      (<ExpenseItem 
-         key={expense.id}    
-         title={expense.title}
-         amount={expense.amount}
-         date={expense.date}
-      />)
-    )
-  };
+  );  
 
   return (
     <div>
       <Card className="expenses">        
-      <ExpensesFilter selected={selectedYear} onYearChange={handleYearChange}/> 
-      {expenseContent}
+        <ExpensesFilter selected={selectedYear} onYearChange={handleYearChange}/> 
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
